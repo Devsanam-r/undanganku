497bb01dd686410fa2088029ee33d9d1f4be2de5
Devsanam-r <devsanam.r@gmail.com>
2024-12-08 07:52:04 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -4,5 +4,8 @@
  //  myModal.show();
 // });
 
-var myModalEl = document.getElementById('sampul-modal')
-var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+//var myModalEl = document.getElementById('sampul-modal')
+//var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
+var myModalEl = document.querySelector('#sampul-modal')
+var modal = bootstrap.Modal.getOrCreateInstance(myModalEl) // Returns a Bootstrap modal instance
+
