bb215bb09502dd95e3fa1b552589880f1f69d01b
Devsanam-r <devsanam.r@gmail.com>
2024-12-08 07:38:46 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,6 @@ const myModal = boostrap.Modal.getOrCreateInstance('#sampul-modal');
 window.addEventListener('DOMContentLoaded', () => {
   myModal.show();
 });
+
+var myModalEl = document.getElementById('myModal')
+var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
