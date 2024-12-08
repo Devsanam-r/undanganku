73e31c63901610f7d50d2ac3d8af9677e007bfb3
Devsanam-r <devsanam.r@gmail.com>
2024-12-08 16:10:41 +0700

    update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-// const myModal = boostrap.Modal.getOrCreateInstance('#sampul-modal');
+const myModal = boostrap.Modal.getOrCreateInstance('#exampleModalFullscreen');
 
-// window.addEventListener('DOMContentLoaded', () => {
- //  myModal.show();
-// });
+window.addEventListener('DOMContentLoaded', () => {
+  myModal.show();
+  });
 
 //var myModalEl = document.getElementById('sampul-modal')
 //var modal = bootstrap.Modal.getInstance(myModalEl) // Returns a Bootstrap modal instance
-var myModalEl = document.querySelector('#sampul-modal')
-var modal = bootstrap.Modal.getOrCreateInstance(myModalEl) // Returns a Bootstrap modal instance
+//var myModalEl = document.querySelector('#sampul-modal')
+//var modal = bootstrap.Modal.getOrCreateInstance(myModalEl) // Returns a Bootstrap modal instance
 
